22a85dca63ef91a00bad90b3fbf3c54baf8f94dd
rokaskl <rokasklkl@gmail.com>
2023-02-14 16:38:17 +0200

    Add Rover and RoverMovementService without implementation

---
+++ JupiterRoverExercise/Rover.cs
@@ -0,0 +1,46 @@
+﻿namespace JupiterRoverExercise;
+
+public interface IRover
+{
+    void Move(MovementDirection movementDirection);
+    void Rotate(RotationDirection rotationDirection);
+}
+public class Rover
+{
+    private readonly Tuple<int, int, Direction> _currentPosition;
+    public Tuple<int, int, Direction> CurrentPosition => _currentPosition;
+
+    public Rover()
+    {
+        _currentPosition = new(0, 0, Direction.N);
+    }
+
+    public void Move(MovementDirection movementDirection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Rotate(RotationDirection rotationDirection)
+    {
+        throw new NotImplementedException();
+    }
+}
+public enum MovementDirection
+{
+    F = 1,
+    B = -1
+}
+
+public enum RotationDirection
+{
+    R = 1,
+    L = -1
+}
+
+public enum Direction
+{
+    N = 0,
+    E = 1,
+    S = 2,
+    W = 3
+}
---
+++ JupiterRoverExercise/RoverMovementService.cs
@@ -0,0 +1,20 @@
+﻿namespace JupiterRoverExercise;
+
+public interface IRoverMovementService
+{
+    void ValidateAndExecuteCommandsFromCommandsString(string commandsString);
+    void ValidateCommandsString(string commandsString);
+}
+
+public class RoverMovementService : IRoverMovementService
+{
+    public void ValidateAndExecuteCommandsFromCommandsString(string commandsString)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ValidateCommandsString(string commandsString)
+    {
+        throw new NotImplementedException();
+    }
+}
