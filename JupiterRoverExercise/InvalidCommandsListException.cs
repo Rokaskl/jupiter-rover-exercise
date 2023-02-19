74ad252e0407ca471b2f682212fc8f5a06734daa
rokaskl <rokasklkl@gmail.com>
2023-02-19 23:32:54 +0200

    Implement MovementService, add InvalidCommandsListException

---
+++ JupiterRoverExercise/InvalidCommandsListException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace JupiterRoverExercise
+{
+    [Serializable]
+    public class InvalidCommandsListException : Exception
+    {
+        public InvalidCommandsListException()
+        {
+        }
+
+        public InvalidCommandsListException(string? message) : base(message)
+        {
+        }
+
+        public InvalidCommandsListException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidCommandsListException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- JupiterRoverExercise/MovementService.cs
+++ JupiterRoverExercise/MovementService.cs
@@ -3,18 +3,57 @@
 public interface IMovementService
 {
     void ValidateAndExecuteCommandsFromCommandsString(string commandsString);
-    (bool,string) ValidateCommandsString(string commandsString);
+    (bool, string) ValidateCommandsString(string commandsString);
 }
 
 public class MovementService : IMovementService
 {
+    private readonly IRover _rover;
+
+    public MovementService(IRover rover)
+    {
+        _rover = rover;
+    }
+
     public void ValidateAndExecuteCommandsFromCommandsString(string commandsString)
     {
-        throw new NotImplementedException();
+        var validationResult = ValidateCommandsString(commandsString);
+        if (!validationResult.Item1) throw new InvalidCommandsListException(validationResult.Item2);
+
+        foreach (char symbol in commandsString.ToCharArray())
+        {
+            if (Enum.TryParse(symbol.ToString(), out MovementCommand movementCommand))
+            {
+                _rover.Move(movementCommand);
+                continue;
+            }
+
+            if (Enum.TryParse(symbol.ToString(), out RotationCommand rotationCommand))
+            {
+                _rover.Rotate(rotationCommand);
+                continue;
+            }
+        }
     }
 
     public (bool, string) ValidateCommandsString(string commandsString)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(commandsString)) return (false, "Commands list is empty");
+
+        //Find invalid commands and build error message
+        var invalidChars = new List<char>();
+        foreach (char symbol in commandsString.ToCharArray())
+        {
+            // symbol is not letter or its not MovementCommand or RotationCommand
+            if (!char.IsLetter(symbol) || IsNotMovementOrRotationCommand(symbol))
+                invalidChars.Add(symbol);
+        }
+        if (invalidChars.Count != 0) return (false, string.Join(' ', "Commands list includes invalid commands :", string.Join(',', invalidChars)));
+
+        return (true, string.Empty);
     }
+
+    private static bool IsNotMovementOrRotationCommand(char symbol) =>
+        !Enum.IsDefined(typeof(MovementCommand), symbol.ToString()) &&
+        !Enum.IsDefined(typeof(RotationCommand), symbol.ToString());
 }
