6b3636da7f282638e80e64abc8836f52b1416e36
rokaskl <rokasklkl@gmail.com>
2023-02-19 21:42:19 +0200

    Added unit tests for ValidateAndExecuteCommandsFromCommandsString method

--- JupiterRoverExerciseTests/MovementServiceTests.cs
+++ JupiterRoverExerciseTests/MovementServiceTests.cs
@@ -4,12 +4,14 @@ namespace JupiterRoverExerciseTests
 {
     public class MovementServiceTests
     {
-        private readonly IMovementService _movementService;
+        private IMovementService _movementService;
+        private Mock<IRover> _roverMock;
 
-        public MovementServiceTests()
+        [SetUp]
+        public void SetupBeforeEachTest()
         {
-            var roverMock = new Mock<IRover>();
-            _movementService = new MovementService(roverMock.Object);
+            _roverMock = new Mock<IRover>();
+            _movementService = new MovementService(_roverMock.Object);
         }
 
         [Test]
@@ -41,6 +43,73 @@ namespace JupiterRoverExerciseTests
             var validationResult = _movementService.ValidateCommandsString(commandsString);
 
             Assert.That(validationResult.Item1, Is.EqualTo(false));
+        }
+
+        [Test]
+        [TestCase("BF")]
+        [TestCase("BFBF")]
+        [TestCase("BFBFBF")]
+        [TestCase("BFBFBFB")]
+        [Parallelizable(ParallelScope.All)]
+        public void ValidateAndExecuteCommandsFromCommandsString_XCommandsBAndF_RoverMoveMethodCalledXTimes(string commandsString)
+        {
+            _movementService.ValidateAndExecuteCommandsFromCommandsString(commandsString);
+
+            _roverMock.Verify(x => x.Move(MovementCommand.B), Times.Exactly(commandsString.Length / 2));
+            _roverMock.Verify(x => x.Move(MovementCommand.F), Times.Exactly(commandsString.Length / 2));
+        }
+
+        [Test]
+        [TestCase("LR")]
+        [TestCase("LRLR")]
+        [TestCase("LRLRLR")]
+        [TestCase("LRLRLRLR")]
+        [Parallelizable(ParallelScope.All)]
+        public void ValidateAndExecuteCommandsFromCommandsString_XCommandsLAndR_RoverRotateMethodCalledXTimes(string commandsString)
+        {
+            _movementService.ValidateAndExecuteCommandsFromCommandsString(commandsString);
+
+            _roverMock.Verify(x => x.Rotate(RotationCommand.L), Times.Exactly(commandsString.Length / 2));
+            _roverMock.Verify(x => x.Rotate(RotationCommand.R), Times.Exactly(commandsString.Length / 2));
+        }
+
+        [Test]
+        [TestCase("L")]
+        [TestCase("LLL")]
+        [TestCase("LLLFFF")]
+        [TestCase("LLLRRRFFFBBB")]
+        [TestCase("RRRBBB")]
+        [TestCase("LRFBBBFR")]
+        [Parallelizable(ParallelScope.All)]
+        public void ValidateAndExecuteCommandsFromCommandsString_ValidXCommands_RoverRotateAndMoveMethodsCalledXTimes(string commandsString)
+        {
+            var Ls = commandsString.Count(x => x.Equals('L'));
+            var Rs = commandsString.Count(x => x.Equals('R'));
+            var Fs = commandsString.Count(x => x.Equals('F'));
+            var Bs = commandsString.Count(x => x.Equals('B'));
+
+            _movementService.ValidateAndExecuteCommandsFromCommandsString(commandsString);
+
+            _roverMock.Verify(x => x.Rotate(RotationCommand.L), Times.Exactly(Ls));
+            _roverMock.Verify(x => x.Rotate(RotationCommand.R), Times.Exactly(Rs));
+            _roverMock.Verify(x => x.Move(MovementCommand.F), Times.Exactly(Fs));
+            _roverMock.Verify(x => x.Move(MovementCommand.B), Times.Exactly(Bs));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("LLLX")]
+        [TestCase("XLLLFFF")]
+        [TestCase("LLLRRXRFFFBBB")]
+        [TestCase("X")]
+        [TestCase("LRFBBBFR'`!.[]()1X")]
+        [Parallelizable(ParallelScope.All)]
+        public void ValidateAndExecuteCommandsFromCommandsString_InValidCommands_RoverRotateAndMoveMethodsNeverCalled(string commandsString)
+        {
+            _movementService.ValidateAndExecuteCommandsFromCommandsString(commandsString);
+
+            _roverMock.Verify(x => x.Move(It.IsAny<MovementCommand>()), Times.Never);
+            _roverMock.Verify(x => x.Rotate(It.IsAny<RotationCommand>()), Times.Never);
         }
     }
 }
