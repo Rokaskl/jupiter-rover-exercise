441473c1f902cca24cfb78f6012ff453b7650e6c
rokaskl <rokasklkl@gmail.com>
2023-02-27 12:34:40 +0200

    Add RoverPossitionModel for responses parsing

---
+++ JupiterRoverExercise/RoverPossitionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JupiterRoverExercise
+{
+    public record struct RoverPossitionModel(int X, int Y, Direction Direction);
+   
+    public static class RoverPossitionModelExtentions
+    {
+        public static RoverPossitionModel FromTupleToRoverPossitionModel(this (int, int, Direction) posstionTuple) => new()
+        { X = posstionTuple.Item1, Y = posstionTuple.Item2, Direction = posstionTuple.Item3 };
+    }
+}
